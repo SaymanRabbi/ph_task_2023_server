08130e65a8ba5574e0b64831b97a792c2d6cde0f
saymanrabbi <mdrabbi.asm@gmail.com>
2023-01-29 14:51:43 +0600

    get all billing

--- Controllers/BillingContoler.js
+++ Controllers/BillingContoler.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const Billing = require('../Models/BillingModel');
 
 
 exports.billingListControler = async (req, res) => {
     try {
-        
+        const billingList = await Billing.find();
+        res.status(200).send({ success: true, data: billingList });
     } catch (error) {
         res.status(500).send({ success: false, messages: error?.message });
     }
--- Routes/BillingRoute.js
+++ Routes/BillingRoute.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const route = require('express').Router();
-
+const BillingControler = require('../Controllers/BillingContoler');
 
 // @route get api/billing-list
 // @desc Get all Billing
 // @access Private
 
-route.get('/billing-list', )
+route.get('/billing-list',BillingControler.billingListControler)
 
 module.exports = route;
