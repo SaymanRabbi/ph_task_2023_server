19300f72606b34ae5606b0b0d726a337db7dc2c4
saymanrabbi <mdrabbi.asm@gmail.com>
2023-01-29 13:06:08 +0600

    user controler

---
+++ Controllers/UserController.js
@@ -0,0 +1,10 @@
+const register = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+            success: false
+        })
+    }
+}
