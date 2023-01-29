4f09fa053f429e787abc06476a666b8b658b1c19
saymanrabbi <mdrabbi.asm@gmail.com>
2023-01-29 15:04:48 +0600

    add middle ware

--- Routes/BillingRoute.js
+++ Routes/BillingRoute.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const route = require('express').Router();
 const BillingControler = require('../Controllers/BillingContoler');
+const { verifyauth } = require('../helpers/CreateToken');
 
 // @route get api/billing-list
 // @desc Get all Billing
 // @access Private
-route.get('/billing-list',BillingControler.billingListControler)
+route.get('/billing-list',verifyauth,BillingControler.billingListControler)
 // @route post api/add-billing
 // @desc add a Billing
 // @access Private
-route.post('/add-billing',BillingControler.addBillingControler)
+route.post('/add-billing',verifyauth,BillingControler.addBillingControler)
 module.exports = route;
--- helpers/CreateToken.js
+++ helpers/CreateToken.js
@@ -1,4 +1,5 @@
   const jwt = require("jsonwebtoken");
+  const {promisify} = require('util');
   // ---------------Generate Token-----------------
   exports.genaretToken = (payload, expire) => {
     return jwt.sign(payload, process.env.SECRET_TOKEN, {
@@ -7,6 +8,21 @@
   };
 
     // ---------------Verify Token-----------------
-    exports.verifyToken = (token) => {
-        // return jwt.verify(token, process.env.SECRET_TOKEN);
+    exports.verifyauth = async (req, res, next) => {
+        try {
+            const token = req.headers?.authorization?.split(" ")[1];
+            if (!token) {
+                return res.status(401).send({
+                    status: false,
+                    message: "Unauthorized"
+                })
+            }
+            const decoded =await promisify(jwt.verify)(token, process.env.SECRET_TOKEN);
+            req.userData = decoded;
+            next();
+        } catch (error) {
+            return res.status(401).json({
+                message: 'Auth failed'
+            });
+        }
     }
