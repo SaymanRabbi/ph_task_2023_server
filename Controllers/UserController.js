b4dc81a7b494d2ee0363e1a95ad1543d49736ade
saymanrabbi <mdrabbi.asm@gmail.com>
2023-01-29 13:21:32 +0600

    hash password

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,7 +1,15 @@
 const userServices = require('../Services/UserServices');
+const bcrypt = require("bcryptjs");
 const register = async (req, res) => {
     try {
         const user = await userServices.registerUserInfo(req.body);
+        // user already exists
+        if (user) {
+            return res.status(400).send({success:false,message:"User Already Exists"});
+        }
+        // user does not exist
+        const {name, email, password} = req.body;
+        const hashPassword = await bcrypt.hash(password, 10);
     } catch (error) {
         res.status(500).json({
             message: error.message,
